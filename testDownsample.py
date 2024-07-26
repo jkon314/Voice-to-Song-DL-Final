88f5ddc9d1f367198ed12cebb3dbf36548cbd368
jkon314 <jkonieczka3@gatech.edu>
2024-07-26 12:19:39 -0400

    Added forward pass test

--- testDownsample.py
+++ testDownsample.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 import torchaudio as ta
 
 
-vocals = ta.load('collardgreens.mp3')
+vocals = ta.load('Tracks/vocals_chunks/001  Iggy Azalea - Fancy (Feat. Charli XCX)_(Vocals)_UVR-MDX-NET-Inst_HQ_3-chunk-1.mp3')
 
 
 
 transform = ta.transforms.Spectrogram(510)
-
+print(vocals[0].shape)
 out = transform(vocals[0])
 
 print(out.shape)
